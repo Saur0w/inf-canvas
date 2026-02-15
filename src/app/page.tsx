7eb4ee300940d6b4f9e7c24700de4113ee1591bf
saur0w <sthapliyal085@gmail.com>
2026-02-15 20:28:44 +0530

    "Added Lenis"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { useState }  from "react";
+import { useState, useEffect }  from "react";
 import Images from "@/images.json";
 import { InfiniteCanvas } from "@/components/Landing";
 import type { MediaItem } from "@/components/Landing/types";
 import { PageLoader } from "@/components/Preloader";
+import Lenis from "lenis";
 
 export default function App() {
+    useEffect(() => {
+        const lenis = new Lenis();
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        }
+    }, []);
     const [media] = useState<MediaItem[]>(Images);
     const [textureProgress, setTextureProgress] = useState(0);
 
